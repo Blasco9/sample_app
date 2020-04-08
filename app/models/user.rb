2fbc72233f362c5cff53b9b7566d2c26133f2a61
Julian Blasco <blascoblasco9@gmail.com>
2020-04-08 00:06:51 -0300

    Create validations for user model attributes and tests for each attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'name', email: 'email')
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = '  '
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = '  '
+    assert_not @user.valid?
+  end
+
+  test 'name should have a maximum length of 30' do
+    @user.name = 'a'*31
+    assert_not @user.valid?
+  end
+
+  test 'email should have a maximum length of 255' do
+    @user.email = 'a'*244 + '@example.com' 
+    assert_not @user.valid?
+  end
 end
