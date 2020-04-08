1edf1dba2faaf14bf9fc5e38312f53c54ba63563
Julian Blasco <blascoblasco9@gmail.com>
2020-04-08 16:58:29 -0300

    Fix rubocop offenses

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,8 +4,8 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: 'name', email: 'name@example.com', password: 'pass123', password_confirmation: 'pass123')
   end
-  
-  test "should be valid" do
+
+  test 'should be valid' do
     assert @user.valid?
   end
 
@@ -20,18 +20,18 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'name should have a maximum length of 30' do
-    @user.name = 'a'*31
+    @user.name = 'a' * 31
     assert_not @user.valid?
   end
 
   test 'email should have a maximum length of 255' do
-    @user.email = 'a'*244 + '@example.com' 
+    @user.email = 'a' * 244 + '@example.com'
     assert_not @user.valid?
   end
 
   test 'email validation should accept valid addresses' do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-    first.last@foo.jp alice+bob@baz.cn]
+                         first.last@foo.jp alice+bob@baz.cn]
 
     valid_addresses.each do |valid_address|
       @user.email = valid_address
@@ -40,15 +40,15 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'email validation should reject invalid addresses' do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. 
-    foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
 
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address} should be invalid"
     end
   end
-  
+
   test 'email addresses should be unique' do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
@@ -56,8 +56,8 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-  test "email addresses should be saved as lower-case" do
-    mixed_case_email = "Foo@ExAMPle.CoM"
+  test 'email addresses should be saved as lower-case' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
